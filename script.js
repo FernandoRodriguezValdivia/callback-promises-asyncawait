1d8674d41e091ea63c1b779f6b6e5d3adf3eb4ff
Fernando Rodriguez <frodriguezv06@gmail.com>
2023-03-03 02:57:53 -0500

    creando un fetchFake2 que usa un .json()
    El metodo .json() retorna una promesa
    
    Tambien se le agregó un parametro a las funciones de busquedaDragonBall para buscar por id

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ const busquedaAsyncAwait = async ( id ) => {
   }
 }
 
-
+// ------------------------------------------------------------------------------
 // Simulando Fetch
 
 const personajes = [
@@ -75,6 +75,7 @@ const personajes = [
   }
 ]
 
+// Fetch con Promise directo
 const fetchFake = (url) => {
   return new Promise(( resolve , reject ) =>{
     const id = Number(url.split('/')[url.split('/').length - 1])
@@ -91,10 +92,40 @@ const fetchFake = (url) => {
   })
 }
 
+// Fetch con Promise que retorna otra promesa
+const fetchFake2 = (url) => {
+  return new Promise(( resolve , reject ) =>{
+    const id = Number(url.split('/')[url.split('/').length - 1])
+    const result = personajes.filter( item => item.id === id )
+    setTimeout(()=>{
+      if( result.length > 0 ){
+        const resultResponse = {
+          data: {info: 'tenemos que convertir esto  a json usando .json() que si le haces un console.log veras que es un promise por que toma tiempo en transformar la informacion'},
+          json: function(){
+            return new Promise( (res, rej) => {
+              setTimeout(()=>{
+                res( result[0] )
+              }, 1000)
+            })
+          }
+        }
+        resolve( resultResponse )
+      } else {
+        const error = new Error('No existe el usuario')
+        error.status = 404  // agregando propiedades al error
+        error.statusText = 'Not Found'
+        // const error = { message: 'No existe el usuario' }
+        reject(error)
+      }
+    },2000)
+  })
+}
+
 // Promises
 
-const busquedaDragonBallPromise = () => {
-  const url = 'http://busquedadragonball/1'
+
+const busquedaDragonBallPromise = (id) => {
+  const url = 'http://busquedadragonball/'+id
   fetchFake(url)
     .then( result => {
       const h1 = document.querySelector('h1')
@@ -104,20 +135,52 @@ const busquedaDragonBallPromise = () => {
       const h1 = document.querySelector('h1')
       h1.textContent = err.message
     })
-}
+  }
 
 // Async Await
 
-const busquedaDragonBallAsyncAwait = async () => {
+const busquedaDragonBallAsyncAwait = async (id) => {
   const h1 = document.querySelector('h1')
   h1.textContent = 'Buscando'
   try {
-    const url = 'http://busquedadragonball/1'
+    const url = 'http://busquedadragonball/'+id
     const result = await fetchFake(url)
     h1.textContent = result.name
   } catch (error) {
+    console.log(error)
     h1.textContent = error.message
   }
 }
 
-busquedaDragonBallAsyncAwait()
+// Usando el fetchFake2 que retorna un objeto con una metodo json que es una promesa
+const busquedaDragonBall = (id) => {
+  const url = 'http://busquedadragonball/'+id
+  const h1 = document.querySelector('h1')
+  let i = 0
+  const interval = setInterval(() => {
+    h1.textContent = 'Buscando personaje con id: '+id+ '.'.repeat(i)
+    if( i >= 3) i=0
+    else i++
+  }, 250)
+  fetchFake2(url)
+    .then( result => { // el result es data que aun no se puede ver si haces un console.log(result) veras informacion de como ver el resultado  ( se debe usar el .json() )
+      // console.log(result)
+      // console.log(result.json())  
+      return result.json()// aca se simula un .json() de los request y se ve que es una promesa
+    })
+    .then( resjson => { // se resuelve la promesa y se obtiene la data
+      h1.textContent = resjson.name
+      clearInterval(interval)
+    } )
+    .catch( err => {
+      console.log(err.status)
+      h1.textContent = err.message
+      clearInterval(interval)
+    })
+}
+
+// modificando funcion para pasarle el id
+// busquedaDragonBallPromise(1) 
+// busquedaDragonBallAsyncAwait(2)
+
+busquedaDragonBall(2)
